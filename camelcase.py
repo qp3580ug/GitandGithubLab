aa4bcbeb19464ed4305a62dfa08e533454734f4a
Nathan W <nathan@email.com>
2019-09-05 18:43:18 -0500

    add program name banner

--- camelcase.py
+++ camelcase.py
@@ -1,19 +1,25 @@
 def camelcase(sentence):
-""" Convert sentence to camelCase, for example, "Display all books" 
-is converted to "displayAllBooks" """
+    """ Convert sentence to camelCase, for example, "Display all books" is converted to "displayAllBooks" """
 
-title_case = sentence.title() # Uppercase first letter of each word
-upper_camel_cased = title_case.replace(' ', '') # remove spaces 
-# Lowercase first letter, join with rest of string 
-# Slices don't produce index out of bounds errors. 
-# So this still works on empty strings, strings of length 1
+    title_case = sentence.title() # Uppercase first letter of each word
+    upper_camel_cased = title_case.replace(' ', '') # remove spaces 
+    # Lowercase first letter, join with rest of string 
+    # Slices don't produce index out of bounds errors. 
+    # So this still works on empty strings, strings of length 1
 
-return upper_camel_cased[0:1].lower() + upper_camel_cased[1:] 
+    return upper_camel_cased[0:1].lower() + upper_camel_cased[1:] 
+
+def display_banner():
+    """ Display program name in banner """
+    msg = 'AWSOME camelCaseGenerator PROGRAM'
+    stars = '*' * len(msg)
+    print(f'\n {stars} \n {msg} \n {stars}\n')
 
 def main():
-sentence = input('Enter your sentence: ')
-output = camelcase(sentence)
-print(output)
+    display_banner()
+    sentence = input('Enter your sentence: ')
+    output = camelcase(sentence)
+    print(output)
 
 if __name__ == '__main__':
-main()
+    main()
